ee42f0c3763bd493da1cfce1c9dfe0c8225740b3
Gioianac <joy.oppliger@heig-vd.ch>
2019-03-28 10:38:24 +0100

    update graph

--- drawGraph.js
+++ drawGraph.js
@@ -8,16 +8,12 @@ const writeSvg = svg =>
 
 const DATA = require('./asylumByCountry.json');
 
-const COLORS = ["black", "#00dac3", "orange", "yellow", "green", "red", "purple", "pink",
-                "grey", "blue", "#0066ff", "#6600cc", "#cc6699", "#ffff00", "#66ffcc", "#ff9900",
-                "#ccffff", "#ff5050", "#006699"]
-
-const graph = DATA => 
-`<svg width="1200" heigth="800" xmlns="http://www.w3.org/2000/svg">
-${DATA.map((d,i) => `<circle cx="${(i+1)*90}" cy="105" r="${d.somme}" fill="${COLORS[i]}" />
-					 <text x="${(i+1)*70}" y="40" font-family="Helvetica" font-size="9">${d.country_asylum}(${d.somme})</text>`).join('\n')}
-					 <text x="60" y="190" font-family="Helvetica" font-size="20">Demandes d'asiles par pays</text>
+const graph = DATA => `<svg width="1800" heigth="800" xmlns="http://www.w3.org/2000/svg">
+	${DATA.map((d,i) => `<circle cx="${(i+1)*87}" cy="120" r="${d.somme}" fill="blue" />
+						<text x="${(i+1)*80}" y="30" font-family="Helvetica" font-size="10">${d.country_asylum}(${d.somme})</text>`).join('\n')}
+						<text x="700" y="220" font-family="Helvetica" font-size="20">Demandes d'asiles par pays</text>
 </svg>`
+
 
 
 writeSvg(graph(DATA));
